67ac4f6c9bfbcfcca2049280d79b0f90e8d2d576
unknown <dlwlgus4553@gmail.com>
2019-12-23 22:36:51 +0900

    start to set form action

--- mangmo3/map/static/js/index.js
+++ mangmo3/map/static/js/index.js
@@ -7,15 +7,7 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 // 커피숍 마커가 표시될 좌표 배열입니다
-var coffeePositions = [ 
-    new kakao.maps.LatLng(37.499590490909185, 127.0263723554437),
-    new kakao.maps.LatLng(37.499427948430814, 127.02794423197847),
-    new kakao.maps.LatLng(37.498553760499505, 127.02882598822454),
-    new kakao.maps.LatLng(37.497625593121384, 127.02935713582038),
-    new kakao.maps.LatLng(37.49646391248451, 127.02675574250912),
-    new kakao.maps.LatLng(37.49629291770947, 127.02587362608637),
-    new kakao.maps.LatLng(37.49754540521486, 127.02546694890695)                
-];
+
 
 //지도가 이동시 이벤트
 kakao.maps.event.addListener(map, 'dragend', function() {        
@@ -64,27 +56,6 @@ function counter(swLatLng, neLatLng, list) {
 
 
 
-// 편의점 마커가 표시될 좌표 배열입니다
-var storePositions = [
-    new kakao.maps.LatLng(37.497535461505684, 127.02948149502778),
-    new kakao.maps.LatLng(37.49671536281186, 127.03020491448352),
-    new kakao.maps.LatLng(37.496201943633714, 127.02959405469642),
-    new kakao.maps.LatLng(37.49640072567703, 127.02726459882308),
-    new kakao.maps.LatLng(37.49640098874988, 127.02609983175294),
-    new kakao.maps.LatLng(37.49932849491523, 127.02935780247945),
-    new kakao.maps.LatLng(37.49996818951873, 127.02943721562295)
-];
-
-// 주차장 마커가 표시될 좌표 배열입니다
-var carparkPositions = [
-    new kakao.maps.LatLng(37.49966168796031, 127.03007039430118),
-    new kakao.maps.LatLng(37.499463762912974, 127.0288828824399),
-    new kakao.maps.LatLng(37.49896834100913, 127.02833986892401),
-    new kakao.maps.LatLng(37.49893267508434, 127.02673400572665),
-    new kakao.maps.LatLng(37.49872543597439, 127.02676785815386),
-    new kakao.maps.LatLng(37.49813096097184, 127.02591949495914),
-    new kakao.maps.LatLng(37.497680616783086, 127.02518427952202)                       
-];    
 
 
 var markerImageSrc = 'http://t1.daumcdn.net/localimg/localimages/07/mapapidoc/category.png';  // 마커이미지의 주소입니다. 스프라이트 이미지 입니다
@@ -243,3 +214,18 @@ function changeMarker(type){
         setCarparkMarkers(map);  
     }    
 } 
+
+
+// 검색시작
+function search(){
+    var loginForm = document.loginForm;
+    var userId = loginForm.userId.value;
+    var password = loginForm.password.value;
+    
+    if(!userId || !password){
+        alert("아이디와 비밀번호를 모두 입력해주세요.")
+    }else{
+        loginForm.submit();
+    }
+}
+
