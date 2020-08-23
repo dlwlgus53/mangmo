03f5d0777981a30a74b319df0ea63b3cb118fd3b
unknown <dlwlgus4553@gmail.com>
2020-08-23 17:54:54 +0900

    prepare to deploy

--- mangmo3/mangmo3/settings.py
+++ mangmo3/mangmo3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7=ol@0+0a2x!$o+t#gly$9v2vir(7_)ihj2c-#0&-ef50q7q_s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
--- mangmo3/map/static/js/search.js
+++ mangmo3/map/static/js/search.js
@@ -28,7 +28,7 @@ geocoder.addressSearch(address, function(result, status) {
     
         // 결과값으로 받은 위치를 마커로 표시합니다
         var imageSrc = "https://i.ibb.co/ZMtxGY8/house.png" // 마커이미지의 주소입니다    
-            imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+            imageSize = new kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
             imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
       
         // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
